0d41988cc3918c49e58ce8e82cee264c69cba889
astroboy <datautrust@yahoo.com>
2020-09-09 12:32:08 -0700

    Update our cloud function. revision 3

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,25 @@
 exports.handler = function(event,context,callback) {
-	callback(null,{
+	let body
+	const  secretContent = `
+	 <h3> Welcome ot the scret area </h3>
+	 <p>here we can tell you the sky is <strong>blue</strong>. </p>
+	`
+	if (event.body) {
+		body = JSON.parse(event.body)
+		
+	} else { 
+		body = {}
+	}
+	
+	if (body.password =="javascript") {
+		callback(null,{
 		statusCode: 200,
-		body: "Welcome to the super scret area"
+		body: secretContent
+		
 		})
+	} else {
+		callback(null,{
+		statusCode: 401
+		})
+	}
 }
