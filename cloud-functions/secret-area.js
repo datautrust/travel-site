6d559ddda7affdc59cbb0b3510be85ff2d6cad22
astroboy <datautrust@yahoo.com>
2020-09-10 09:37:12 -0700

    modified secret-area.js to test locally

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,5 @@
 exports.handler = function(event,context,callback) {
+/*this is the good code to run cloud functions on netlify. the code below is to test locally Sec 20.64 CORS soln
 	let body
 	const  secretContent = `
 	 <h3> Welcome ot the scret area </h3>
@@ -22,4 +23,49 @@ exports.handler = function(event,context,callback) {
 		statusCode: 401
 		})
 	}
+	
+ end code for using netlify to test*/
+ 
+ // local test code
+  const headers = {
+    "Access-Control-Allow-Origin" : "*",
+    "Access-Control-Allow-Headers" : "Content-Type"
+  }
+ 
+  if (event.httpMethod !== "POST") {
+    return callback(null, {
+      statusCode: 200,
+      headers,
+      body: "This was not a POST request"
+    })
+  }
+  
+  const secretContent = `
+  <h3>Welcome To The Secret Area</h3>
+  <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+  `
+  
+  let body
+ 
+  if (event.body) {
+    body = JSON.parse(event.body)
+  } else {
+    body = {}
+  }
+ 
+  if (body.password == "javascript") {
+    callback(null, {
+      statusCode: 200,
+      headers,
+      body: secretContent
+    })
+  } else {
+    callback(null, {
+      statusCode: 401,
+      headers
+    })
+  }
+ 
+ 
+ 
 }
